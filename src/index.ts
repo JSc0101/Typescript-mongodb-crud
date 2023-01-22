2799bfa4d3827d6821ce2e6f9a9054315381c9e2
Sebastian009w <ia3266505@gmail.com>
2023-01-22 12:05:57 -0500

    Se guarda el primer producto en la base de datos

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import mongoose from "mongoose";
+import Products from "./models/products";
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 const connectDB = async () => {
   try {
     const db = await mongoose.connect("mongodb://127.0.0.1:27017/crud-type");
-    console.log('Connection Succesfully to: ', db.connection.db.databaseName);
+    console.log("Connection Succesfully to: ", db.connection.db.databaseName);
   } catch (error) {
     throw error;
   }
 };
 connectDB();
+
+const saveProducts = async () => {
+  const products = new Products({
+    name: "Lapto",
+    price: 3000,
+    amount: 4,
+    date: "1991/09/24",
+  });
+  await products.save();
+  console.log(products);
+};
+saveProducts();
