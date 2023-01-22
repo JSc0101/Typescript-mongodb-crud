35983a597d97c765bbe996e0bd566379f32f644b
Sebastian009w <ia3266505@gmail.com>
2023-01-22 11:42:39 -0500

    Nos conectamos a mongodb a traves del modulo moongose

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('work')
+import mongoose from "mongoose";
+
+mongoose.set('strictQuery', true);
+const connectDB = async () => {
+  try {
+    const db = await mongoose.connect("mongodb://127.0.0.1:27017/crud-type");
+    console.log('Connection Succesfully to: ', db.connection.db.databaseName);
+  } catch (error) {
+    throw error;
+  }
+};
+connectDB();
